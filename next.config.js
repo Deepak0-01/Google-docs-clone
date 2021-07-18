2f77a1327025ab3ecfc7f0d9749e2f7bc3489e6c
Deepak1-11 <ninjaweb22@gmail.com>
2021-07-18 23:15:21 +0530

    fixed env

--- next.config.js
+++ next.config.js
@@ -2,4 +2,10 @@ module.exports = {
     images: {
       domains: ['links.papareact.com'],
     },
+    env: {
+      GOOGLE_CLIENT_ID:"922596999225-lsho0nqi49og6h6lulc67du2if05rcd7.apps.googleusercontent.com",
+     
+GOOGLE_CLIENT_SECRET:"sQDRVeIBz0ul-vFk1YYwmzyt"
+
+    },
   }
--- pages/index.js
+++ pages/index.js
@@ -1,183 +1,153 @@
-import Head from 'next/head'
-import Header from '../components/Header'
+import Head from "next/head";
+import Header from "../components/Header";
 import Icon from "@material-tailwind/react/Icon";
-import Button from '@material-tailwind/react/Button';
-import Image from 'next/image';
-import { getSession, useSession } from 'next-auth/client';
-import Login from '../components/login';
+import Button from "@material-tailwind/react/Button";
+import Image from "next/image";
+import { getSession, useSession } from "next-auth/client";
+import Login from "../components/login";
 import Modal from "@material-tailwind/react/Modal";
 import ModalHeader from "@material-tailwind/react/ModalHeader";
 import ModalBody from "@material-tailwind/react/ModalBody";
 import ModalFooter from "@material-tailwind/react/ModalFooter";
-import { useState } from 'react';
+import { useState } from "react";
 import db from "../firebase";
 import firebase from "firebase";
-import { useCollection, useCollectionOnce } from 'react-firebase-hooks/firestore';
-import DocumentRow from '../components/DocumentRow';
+import {
+  useCollection,
+  useCollectionOnce,
+} from "react-firebase-hooks/firestore";
+import DocumentRow from "../components/DocumentRow";
 
- function Home() {
-
+function Home() {
   const [session] = useSession();
 
-  if(!session)
-  return <Login/>
+  if (!session) return <Login />;
 
+  const [input, setInput] = useState("");
+  const [showModal, setShowModal] = useState(false);
 
+  const [snapshot] = useCollection(
+    db
+      .collection("userDocs")
+      .doc(session?.user.email)
+      .collection("docs")
+      .orderBy("timestamp", "desc")
+  );
 
- const [input, setInput] = useState("");
- const [showModal, setShowModal] = useState(false);
-
- const [snapshot]  = useCollection(db.collection("userDocs").doc(session?.user.email).collection("docs").orderBy("timestamp","desc"));
-
- 
-  
-
-    const handleSubmit = ()=>{
-
-      if(!input)
-      {
-        alert("Please Enter Document Name");
-        return;
-      }
-
-      db.collection("userDocs").doc(session.user.email).collection("docs").add({
-
-        filename:input,
-        timestamp:firebase.firestore.FieldValue.serverTimestamp()
-      })
-     
-      setInput("");
-      setShowModal(false);
+  const handleSubmit = () => {
+    if (!input) {
+      alert("Please Enter Document Name");
+      return;
     }
 
-    console.log(session.user.email);
-    const modal = (
+    db.collection("userDocs").doc(session.user.email).collection("docs").add({
+      filename: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
 
-      <Modal  size="regular" active={showModal} toggler={() => setShowModal(false)}>
+    setInput("");
+    setShowModal(false);
+  };
+
+
+  const modal = (
+    <Modal
+      size="regular"
+      active={showModal}
+      toggler={() => setShowModal(false)}
+    >
       <ModalHeader toggler={() => setShowModal(false)}>
-          New Document
+        New Document
       </ModalHeader>
       <ModalBody className="focus-within:text-gray-300 border-transparent">
-         <input placeholder="Enter Document Name" type="text" className="border-transparent decoration-none px-2 bg-gray-200 h-10 w-70 text-gray-700" onChange={e=>setInput(e.target.value)} />
+        <input
+          placeholder="Enter Document Name"
+          type="text"
+          className="border-transparent decoration-none px-2 bg-gray-200 h-10 w-70 text-gray-700"
+          onChange={(e) => setInput(e.target.value)}
+        />
       </ModalBody>
       <ModalFooter>
-          <Button 
-              color="blue"
-              buttonType="outline"
-              onClick={(e) => setShowModal(false)}
-              ripple="dark"
-          >
-              Close
-          </Button>
+        <Button
+          color="blue"
+          buttonType="outline"
+          onClick={(e) => setShowModal(false)}
+          ripple="dark"
+        >
+          Close
+        </Button>
 
-          <Button
-              color="blue"
-              onClick={handleSubmit}
-              ripple="light"
-          >
-              Create
-          </Button>
+        <Button color="blue" onClick={handleSubmit} ripple="light">
+          Create
+        </Button>
       </ModalFooter>
-  </Modal>
+    </Modal>
+  );
 
-
-    )
-   
-
-    
   return (
-
-    
-    <div >
-  
-         <Head>
+    <div>
+      <Head>
         <title>Google Docs Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header/>
+      <Header />
       {modal}
       <section className="ml-5 bg-[#F1F3F4] py-8">
+        <div className="max-w-3xl mx-auto ">
+          <div className="flex items-center justify-between my-4">
+            <h2 className="text-gray-600">Start a new document</h2>
+            <Button
+              color="gray"
+              buttonType="outline"
+              size="regular"
+              rounded={true}
+              block={false}
+              iconOnly={true}
+              ripple="dark"
+              className=" h-20 w-20 border-0"
+            >
+              <Icon name="more_vert" size="3xl" />
+            </Button>
+          </div>
 
-      <div className="max-w-3xl mx-auto ">
+          <div
+            onClick={(e) => setShowModal(true)}
+            className="relative h-52 w-40 border-2 hover:border-blue-600 cursor-pointer "
+          >
+            <Image src="https://links.papareact.com/pju" layout="fill" />
+          </div>
 
-      <div className="flex items-center justify-between my-4">
-      <h2 className="text-gray-600">Start a new document</h2>
-      <Button
-      color="gray"
-      buttonType="outline"
-      size="regular"
-      rounded={true}
-      block={false}
-      iconOnly={true}
-      ripple="dark"
-      className=" h-20 w-20 border-0"
-  > 
-      <Icon name="more_vert" size="3xl"/>
-      </Button>
-      </div>
-      
-      <div onClick={(e)=> setShowModal(true)} className="relative h-52 w-40 border-2 hover:border-blue-600 cursor-pointer ">
-      <Image src="https://links.papareact.com/pju" 
-
-      layout="fill"
-      
-      />
-
-      </div>
-
-      <p className="ml-2 mt-2 text-gray-600 text-sm font-semibold">Blank</p>
-      
-      </div>
-      
-      
+          <p className="ml-2 mt-2 text-gray-600 text-sm font-semibold">Blank</p>
+        </div>
       </section>
       <section className="max-w-3xl m-auto bg-white py-10">
-      <div className="flex items-center justify-between text-sm text-gray-700 ">
-     <h2 className="font-medium flex-grow">My Documents</h2>
-     <h2 className="font-medium mr-5">Date Created</h2>
-     <Icon name="folder" size="3xl" color="gray"/>
-
- 
-     
-      </div>
-
-     
-
-
-      
-      
+        <div className="flex items-center justify-between text-sm text-gray-700 ">
+          <h2 className="font-medium flex-grow">My Documents</h2>
+          <h2 className="font-medium mr-5">Date Created</h2>
+          <Icon name="folder" size="3xl" color="gray" />
+        </div>
       </section>
 
-      
-     {snapshot?.docs.map((doc)=> (<DocumentRow
-      key={doc.id}
-      id={doc.id}
-      file={doc.data().filename}
-      date={doc.data().timestamp}
-      />
+      {snapshot?.docs.map((doc) => (
+        <DocumentRow
+          key={doc.id}
+          id={doc.id}
+          file={doc.data().filename}
+          date={doc.data().timestamp} 
+        />
       ))}
-
-
-    
-
-    
     </div>
-
-    
-  )
+  );
 }
 
 export default Home;
 
-export async function getServerSideProps(context){
-
+export async function getServerSideProps(context) {
   const session = await getSession(context);
 
-
-  return{
-      props:{
-          session,
-        
-      },
-  }
+  return {
+    props: {
+      session,
+    },
+  };
 }
